e3f0ed9a8700053a5aa3e69999332707a1acb04a
oleksandrosypenko <Oleksandrosypenko1989@gmail.com>
2023-05-10 18:38:06 +0200

    Added commands to the bot menu

--- src/main/java/com/osypenko/suppliermonitoringtelegrambot/service/TelegramBot.java
+++ src/main/java/com/osypenko/suppliermonitoringtelegrambot/service/TelegramBot.java
@@ -1,20 +1,49 @@
 package com.osypenko.suppliermonitoringtelegrambot.service;
 
 import com.osypenko.suppliermonitoringtelegrambot.config.BotConfig;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Component
-@AllArgsConstructor
 public class TelegramBot extends TelegramLongPollingBot { //TODO implement WebhookBoot - реагирует на то что написал пользователь
 
     final BotConfig config;
+    static final String HELP_TEXT = """
+            This bot is created to demonstrate Spring capabilities.\s
+            You can execute commands from the main menu on yhe left or by typing a command:\s
+
+            Type /start to see a welcome message\s
+
+            Type /myData to see data stored about yourself
+
+            Type /help to see this message again""";
+
+    public TelegramBot(BotConfig config) {
+        this.config = config;
+        List<BotCommand> listOfCommands = new ArrayList<>();
+        listOfCommands.add(new BotCommand("/start", "get a welcome message"));
+        listOfCommands.add(new BotCommand("/myData", "get your data stored"));
+        listOfCommands.add(new BotCommand("/deleteData", "delete my data"));
+        listOfCommands.add(new BotCommand("/help", "info how to use this bot"));
+        listOfCommands.add(new BotCommand("/settings", "set your preferences"));
+        try{
+            this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
+            // TODO при запуске отображается ета ошибка
+            log.error("Error setting bot's command list: " + e.getMessage());
+        }
+    }
 
     @Override
     public String getBotUsername() {
@@ -34,6 +63,7 @@ public class TelegramBot extends TelegramLongPollingBot { //TODO implement Webho
 
             switch (messageText) {
                 case "/start" -> startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                case "/help" -> sendMessage(chatId, HELP_TEXT);
                 default -> sendMessage(chatId, "Sorry, command was not recognized");
             }
         }
