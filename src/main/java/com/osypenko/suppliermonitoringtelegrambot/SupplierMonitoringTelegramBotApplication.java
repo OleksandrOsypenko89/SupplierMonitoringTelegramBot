65e5fab21402ff91ed6dbe37ea2dbac888966213
oleksandrosypenko <Oleksandrosypenko1989@gmail.com>
2023-05-10 12:35:28 +0200

    Start TelegramBot project

---
+++ src/main/java/com/osypenko/suppliermonitoringtelegrambot/SupplierMonitoringTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.osypenko.suppliermonitoringtelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplierMonitoringTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupplierMonitoringTelegramBotApplication.class, args);
+    }
+
+}
